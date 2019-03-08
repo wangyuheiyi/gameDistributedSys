17a83a4d78957b3b085e28da86911bcf8c3666e9
wangy <wangy@shanggame.com>
2019-03-08 15:36:38 +0800

    添加 gamelogservice 日志服务

---
+++ gamelogservice/src/main/java/com/gamelogservice/GamelogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gamelogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelogserviceApplication.class, args);
+	}
+
+}
