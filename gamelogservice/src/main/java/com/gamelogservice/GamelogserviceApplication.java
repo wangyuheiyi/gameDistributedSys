80792b6a16db0b3b351fa5780fa4252022fd0ec6
wangy <wangy@shanggame.com>
2019-03-13 14:00:44 +0800

    添加自定义的接口设置

--- gamelogservice/src/main/java/com/gamelogservice/GamelogserviceApplication.java
+++ gamelogservice/src/main/java/com/gamelogservice/GamelogserviceApplication.java
@@ -2,10 +2,16 @@ package com.gamelogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.gamelogservice.dao.BaseRepositoryFactoryBean;
+//指定自己的工厂类
+@EnableJpaRepositories(basePackages ="com.gamelogservice.dao",repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
 @SpringBootApplication
 public class GamelogserviceApplication {
 
+	 
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(GamelogserviceApplication.class, args);
 	}
