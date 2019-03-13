68acf584efc787385483ea5b3704126341a5779e
wangy <wangy@shanggame.com>
2019-03-13 14:01:47 +0800

    去掉没用的类

--- gamelogservice/src/main/java/com/gamelogservice/manageserver/dao/IGameLogManageDao.java
+++ gamelogservice/src/main/java/com/gamelogservice/manageserver/dao/IGameLogManageDao.java
@@ -1,13 +1,12 @@
 package com.gamelogservice.manageserver.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.gamelogservice.dao.repository.IBaseJpaRepository;
 import com.gamelogservice.manageserver.entity.GameLogManageEntity;
 /**
  * dao层主要用于添加某种数据库查询的方式 JpaRepository(jpa的数据持久层)
  * @author Administrator
  *
  */
-public interface IGameLogManageDao extends JpaRepository<GameLogManageEntity, Long>{
+public interface IGameLogManageDao extends IBaseJpaRepository<GameLogManageEntity, Long>{
 
 }
