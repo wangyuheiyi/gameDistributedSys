c2b2b1c84a65c87478f195e5d3989b8c244a7a5d
wangy <wangy@shanggame.com>
2019-03-18 14:26:59 +0800

    添加字段唯一性约束

--- gamelogservice/src/main/java/com/gamelogservice/manageserver/entity/GameLogManageEntity.java
+++ gamelogservice/src/main/java/com/gamelogservice/manageserver/entity/GameLogManageEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(indexes = {@Index(name="INDEX_UID",columnList = "id")})
+@Table(indexes = {@Index(name="INDEX_ID",columnList = "id")},uniqueConstraints = {@UniqueConstraint(columnNames="logservicename")})
 @Entity(name = "t_gamelog_manage")
 public class GameLogManageEntity {
 	@Id
@@ -29,10 +30,10 @@ public class GameLogManageEntity {
 	private int gamecode;
 	
 	/** 游戏名称*/
-	@Column(length = 300,nullable = false,updatable = false)
+	@Column(length = 100,nullable = false,updatable = false)
 	private String gamename;
 
 	/** 日志服名称*/
-	@Column(length = 300,nullable = false)
+	@Column(length = 100,nullable = false)
 	private String logservicename;
 }
